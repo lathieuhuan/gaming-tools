bac41c995b3423f4dad01f6160e8374e03e62cac
huan.la <huan.la@onemount.com>
2024-03-11 18:42:33 +0700

    build(gidmgcalculator): setup tailwind & redux store

--- packages/gidmgcalculator/src/main.tsx
+++ packages/gidmgcalculator/src/main.tsx
@@ -1,4 +1,18 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { DynamicStoreProvider } from "./features";
 import App from "./App.tsx";
+import "./assets/css/tailwind.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <DynamicStoreProvider>
+    {({ store, persistor }) => (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    )}
+  </DynamicStoreProvider>
+);
