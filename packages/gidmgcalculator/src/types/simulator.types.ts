1d94a7aa939beccdec1691df6f8e5b12444048fb
lathieuhuan <lathieuhuan52@gmail.com>
2024-06-05 00:11:24 +0700

    update simulator

--- packages/gidmgcalculator/src/types/simulator.types.ts
+++ packages/gidmgcalculator/src/types/simulator.types.ts
@@ -1,31 +1,55 @@
 import type { Artifact, Character, Weapon } from "./global.types";
 import type { Target } from "./calculator.types";
+import { AttackBonusKey, AttackBonusType, AttributeStat } from "@Src/backend/types";
 
 export type SimulationManageInfo = {
   id: number;
   name: string;
 };
 
-type TriggerEvent = {
+type ModifyEvent = {
   character: string;
   modId: number;
   modInputs: number[];
 };
 
-type ActionEvent = {
+type AttackEvent = {
   character: string;
+  duration: number;
+};
+
+type SimulationBonus = {
+  trigger: {
+    character: string;
+    src: string;
+  };
+  value: number;
+};
+
+export type SimulationAttributeBonus = SimulationBonus & {
+  stable: boolean;
+  to: AttributeStat;
+};
+
+export type SimulationAttackBonus = SimulationBonus & {
+  to: {
+    type: AttackBonusType;
+    key: AttackBonusKey;
+  };
 };
 
 export type SimulationMember = Character & {
   weapon: Weapon;
   artifacts: Array<Artifact | null>;
+  attributeBonus: SimulationAttributeBonus[];
+  attackBonus: SimulationAttackBonus[];
 };
 
 export type SimulationTarget = Target;
 
 export type Simulation = {
   members: SimulationMember[];
-  triggerEvents: TriggerEvent[];
-  actionEvents: ActionEvent[];
+  modifyEvents: ModifyEvent[];
+  attackEvents: AttackEvent[];
   target: SimulationTarget;
 };
