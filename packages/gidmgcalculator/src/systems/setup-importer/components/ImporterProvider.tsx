84ba4afb69393d5f4167a3797e513f834148a470
lathieuhuan <lathieuhuan52@gmail.com>
2025-08-30 12:00:49 +0700

    update

--- packages/gidmgcalculator/src/systems/setup-importer/components/ImporterProvider.tsx
+++ packages/gidmgcalculator/src/systems/setup-importer/components/ImporterProvider.tsx
@@ -1,11 +1,11 @@
 import { ReactNode, useMemo, useState } from "react";
 
+import { SetupImportInfo } from "@Src/types";
 import { ImporterContext } from "../contexts/ImporterContext";
-import { ImportInfo } from "../types";
 import { SetupImportCenter } from "./ImportCenter";
 
 export function ImporterProvider({ children }: { children: ReactNode }) {
-  const [importInfo, setImportInfo] = useState<ImportInfo>({});
+  const [importInfo, setImportInfo] = useState<SetupImportInfo>({});
 
   const value = useMemo(() => {
     return {
