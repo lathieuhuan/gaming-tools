2ae2debb465a34ece32e57c175efa6d4b2b6b28c
lathieuhuan <lathieuhuan52@gmail.com>
2025-09-20 18:59:20 +0700

    refactor

--- packages/gidmgcalculator/src/screens/Calculator/ContextProvider/TeamDataProvider/TeamDataProvider.tsx
+++ packages/gidmgcalculator/src/screens/Calculator/ContextProvider/TeamDataProvider/TeamDataProvider.tsx
@@ -1,15 +1,29 @@
+import { CalcTeamData, TeamData } from "@Calculation";
 import { useMemo } from "react";
 
-import type { Teammates } from "@/types";
+import type { AppCharactersByName, Character, Teammates } from "@/types";
 
-import { CalcTeamData } from "@Calculation";
+import { $AppCharacter } from "@/services";
 import Array_ from "@/utils/Array";
-import { makeTeamData } from "@/utils/makeTeamData";
 import { selectCharacter, selectTeammates } from "@Store/calculator-slice";
 import { useSelector } from "@Store/hooks";
 import { CalcTeamDataContext, TeamDataContext } from "./context";
 
 const emptyTeammates: Teammates = [];
+
+function makeTeamData(character: Character, teammates: Teammates = []) {
+  const appCharacters: AppCharactersByName = {
+    [character.name]: $AppCharacter.get(character.name),
+  };
+  let teammateNames: string[] = [];
+
+  for (const teammate of Array_.truthify(teammates)) {
+    appCharacters[teammate.name] = $AppCharacter.get(teammate.name);
+    teammateNames.push(teammate.name);
+  }
+
+  return new TeamData(character.name, teammateNames, appCharacters);
+}
 
 export function TeamDataProvider(props: { children: React.ReactElement }) {
   const character = useSelector(selectCharacter);
@@ -23,12 +37,16 @@ export function TeamDataProvider(props: { children: React.ReactElement }) {
   }, [character?.name, teammateNames]);
 
   const calcTeamData = useMemo(() => {
-    return character && teamData ? new CalcTeamData(character, teammates, teamData.data) : undefined;
+    return character && teamData
+      ? new CalcTeamData(character, teammates, teamData.data)
+      : undefined;
   }, [character, teammates, teamData]);
 
   return (
     <TeamDataContext.Provider value={teamData}>
-      <CalcTeamDataContext.Provider value={calcTeamData}>{props.children}</CalcTeamDataContext.Provider>
+      <CalcTeamDataContext.Provider value={calcTeamData}>
+        {props.children}
+      </CalcTeamDataContext.Provider>
     </TeamDataContext.Provider>
   );
 }
