dcd40062d622123331c024aee77192dd825be650
lathieuhuan <lathieuhuan52@gmail.com>
2025-09-28 23:34:33 +0700

    fix(rond): BottomSheet not slide up on open

--- packages/rond/lib/components/BottomSheet/BottomSheet.tsx
+++ packages/rond/lib/components/BottomSheet/BottomSheet.tsx
@@ -2,16 +2,16 @@ import { cn } from "@lib/utils";
 import { CloseButton } from "../Button";
 import { Overlay, type OverlayProps } from "../Overlay";
 
-export interface BottomSheetProps
-  extends Pick<
-    OverlayProps,
-    "active" | "transitionDuration" | "closable" | "closeOnMaskClick" | "onClose"
-  > {
+export type BottomSheetProps = Pick<
+  OverlayProps,
+  "active" | "transitionDuration" | "closable" | "closeOnMaskClick" | "onClose"
+> & {
   title: React.ReactNode;
   bodyCls?: string;
   height?: "auto" | "30%" | "50%" | "70%" | "90%";
   children?: React.ReactNode;
-}
+};
+
 export function BottomSheet({
   title,
   bodyCls,
@@ -27,7 +27,7 @@ export function BottomSheet({
             "absolute bottom-0 left-0 w-full rounded-t-xl bg-dark-2 text-white shadow-popup flex flex-col overflow-hidden",
             direction === "out" ? "translate-y-0" : "translate-y-full"
           )}
-          style={{ height, transitionProperty: "transform", ...transitionStyle }}
+          style={{ height, transitionProperty: "translate", ...transitionStyle }}
         >
           <div className="p-1 bg-dark-1 text-heading min-h-10 flex justify-between items-center">
             <div className="px-3 text-lg leading-5.5 font-semibold">{title}</div>
