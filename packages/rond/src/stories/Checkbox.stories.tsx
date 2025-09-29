c64bb620e08dbf24e711cbe9f9a374d9b9493d73
lathieuhuan <lathieuhuan52@gmail.com>
2025-09-30 01:43:18 +0700

    feat(rond): migrate Checkbox style use tailwind

--- packages/rond/src/stories/Checkbox.stories.tsx
+++ packages/rond/src/stories/Checkbox.stories.tsx
@@ -9,6 +9,8 @@ const meta = {
   },
   argTypes: {
     size: { control: "radio", options: ["small", "medium"] },
+    disabled: { control: "boolean" },
+    indeterminate: { control: "boolean" },
   },
   tags: ["autodocs"],
   render: (args) => {
@@ -27,6 +29,8 @@ export const Default: Story = {
   args: {
     size: "small",
     checked: false,
+    indeterminate: false,
+    disabled: false,
     children: "Checkbox",
   },
 };
