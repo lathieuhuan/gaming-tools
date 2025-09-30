d6eb2ad170dcc2c5ee1c8e4c5156dc1fbf8f9da4
lathieuhuan <lathieuhuan52@gmail.com>
2025-09-30 19:09:15 +0700

    feat(rond): migrate CollapseList style use tailwind

--- packages/rond/lib/components/CollapseList/CollapseList.tsx
+++ packages/rond/lib/components/CollapseList/CollapseList.tsx
@@ -1,31 +1,33 @@
 import clsx, { ClassValue } from "clsx";
 import { useState } from "react";
-import { CollapseSpace } from "../CollapseSpace";
-import "./CollapseList.styles.scss";
 
-export interface CollapseListProps {
-  className?: ClassValue;
+import { cn } from "@lib/utils";
+import { CollapseSpace } from "../CollapseSpace";
+
+export type CollapseListProps = {
+  className?: string;
   bodyCls?: ClassValue;
   items: Array<{
     title?: string;
     heading: React.ReactNode | ((expanded?: boolean) => React.ReactNode);
     body: React.ReactNode;
   }>;
-}
+};
+
 export const CollapseList = (props: CollapseListProps) => {
   const [expanded, setExpanded] = useState<(boolean | undefined)[]>([]);
 
   return (
-    <div className={clsx("ron-collapse-list", props.className)}>
+    <div className={props.className}>
       {props.items.map(({ title, heading, body }, i) => {
         const active = !!expanded[i];
 
         return (
-          <div key={i} className="ron-collapse-item">
+          <div key={i} className="mb-3">
             <div
               className={clsx(
-                "ron-collapse-item__heading ron-glow-on-hover",
-                expanded[i] && "ron-collapse-item__heading--active"
+                "pt-1 px-6 pb-0 font-semibold leading-[1.625] cursor-pointer transition-all duration-200 glow-on-hover",
+                "text-light-2 bg-dark-3 aria-expanded:text-black aria-expanded:bg-primary-2"
               )}
               onClick={() =>
                 setExpanded((prev) => {
@@ -41,7 +43,7 @@ export const CollapseList = (props: CollapseListProps) => {
               {typeof heading === "function" ? heading(expanded[i]) : heading}
             </div>
             <CollapseSpace active={active}>
-              <div className={clsx("ron-collapse-item__body", props.bodyCls)}>{body}</div>
+              <div className={cn("pt-2 pr-4 pl-2", props.bodyCls)}>{body}</div>
             </CollapseSpace>
           </div>
         );
