37efdde70debc6f777a7f3fadab14b4a229a7679
lathieuhuan <lathieuhuan52@gmail.com>
2025-10-03 00:36:23 +0700

    move theme to a new package

--- packages/rond/.storybook/preview.ts
+++ packages/rond/.storybook/preview.ts
@@ -13,10 +13,10 @@ const preview: Preview = {
     },
     backgrounds: {
       options: {
-        dark1: { name: 'Dark 1', value: 'var(--dark-1)' },
-        dark2: { name: 'Dark 2', value: 'var(--dark-2)' },
-        dark3: { name: 'Dark 3', value: 'var(--dark-3)' },
-        light: { name: 'Light', value: 'var(--light-1)' },
+        dark1: { name: 'Dark 1', value: 'var(--color-dark-1)' },
+        dark2: { name: 'Dark 2', value: 'var(--color-dark-2)' },
+        dark3: { name: 'Dark 3', value: 'var(--color-dark-3)' },
+        light: { name: 'Light', value: 'var(--color-light-1)' },
       },
     },
   },
