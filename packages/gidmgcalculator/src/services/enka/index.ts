802b4c07a1cb4cb61db803561463b44f7287205b
lathieuhuan <lathieuhuan52@gmail.com>
2026-01-12 01:07:40 +0700

    deploy 1/11/2026

--- packages/gidmgcalculator/src/services/enka/index.ts
+++ packages/gidmgcalculator/src/services/enka/index.ts
@@ -1,5 +1,5 @@
 import { IS_DEV_ENV } from "@/constants/config";
-import { mock } from "./mock";
+// import { mock } from "./mock";
 import { GenshinUserResponse } from "./types";
 import { transformResponse } from "./transform";
 import IdStore from "@/utils/IdStore";
@@ -10,43 +10,43 @@ export * from "./types";
 const baseUrl = IS_DEV_ENV ? "http://localhost:3001" : "https://gicalculator.ronqueroc.com";
 
 export async function getGenshinUser(uid: string) {
-  const timeStart = Date.now();
-  const idStore = new IdStore(timeStart);
+  // const timeStart = Date.now();
+  // const idStore = new IdStore(timeStart);
 
-  const user = transformResponse(mock, idStore);
-  const timeEnd = Date.now();
-  const elapsedTime = timeEnd - idStore.latest;
+  // const user = transformResponse(mock, idStore);
+  // const timeEnd = Date.now();
+  // const elapsedTime = timeEnd - idStore.latest;
 
-  // If the number of ids generated is more than the time this transformResponse took,
-  // we need to wait to avoid generating duplicate ids
-  if (elapsedTime < 0) {
-    await delay(-elapsedTime);
-  }
-
-  return user;
-
-  // const response = await fetch(`${baseUrl}/enka/uid/${uid}`);
-
-  // if (response.ok) {
-  //   const res: GenshinUserResponse = await response.json();
-
-  //   const timeStart = Date.now();
-  //   const idStore = new IdStore(timeStart);
-
-  //   const user = transformResponse(res, idStore);
-  //   const timeEnd = Date.now();
-  //   const elapsedTime = timeEnd - idStore.latest;
-
-  //   // If the number of ids generated is more than the time this transformResponse took,
-  //   // we need to wait to avoid generating duplicate ids
-  //   if (elapsedTime < 0) {
-  //     await delay(-elapsedTime);
-  //   }
-
-  //   return user;
+  // // If the number of ids generated is more than the time this transformResponse took,
+  // // we need to wait to avoid generating duplicate ids
+  // if (elapsedTime < 0) {
+  //   await delay(-elapsedTime);
   // }
 
-  // throw await response.json();
+  // return user;
+
+  const response = await fetch(`${baseUrl}/enka/uid/${uid}`);
+
+  if (response.ok) {
+    const res: GenshinUserResponse = await response.json();
+
+    const timeStart = Date.now();
+    const idStore = new IdStore(timeStart);
+
+    const user = transformResponse(res, idStore);
+    const timeEnd = Date.now();
+    const elapsedTime = timeEnd - idStore.latest;
+
+    // If the number of ids generated is more than the time this transformResponse took,
+    // we need to wait to avoid generating duplicate ids
+    if (elapsedTime < 0) {
+      await delay(-elapsedTime);
+    }
+
+    return user;
+  }
+
+  throw await response.json();
 }
 
 export async function updateCache() {
