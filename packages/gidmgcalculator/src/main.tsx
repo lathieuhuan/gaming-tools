a0cc5662a9a8dc928f3dd2ee41accbed3a0d8e74
lathieuhuan <lathieuhuan52@gmail.com>
2026-07-20 14:37:03 +0700

    revert settingsStore migration

--- packages/gidmgcalculator/src/main.tsx
+++ packages/gidmgcalculator/src/main.tsx
@@ -1,12 +1,12 @@
 import { enableMapSet } from "immer";
 import ReactDOM from "react-dom/client";
-// import { Provider as StoreProvider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
+import { Provider as StoreProvider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { ConfigProvider, ScreenSizeWatcher } from "rond";
 
 import { route } from "./app/route";
 import { GenshinImage } from "./components";
-// import { DynamicStoreProvider } from "./lib/dynamic-store";
+import { DynamicStoreProvider } from "./lib/dynamic-store";
 import { QueryClientProvider } from "./lib/react-query";
 import { RouterProvider } from "./lib/router";
 
@@ -19,23 +19,16 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <ScreenSizeWatcher>
     <ConfigProvider config={{ ImageFallback: GenshinImage.Fallback }}>
       <QueryClientProvider>
-        {/* TODO: 
-          - Undo after 1/6/2026
-          - Also check App.tsx
-        */}
-        <RouterProvider route={route} />
-        {/* 
-          <DynamicStoreProvider>
-            {({ store, persistor }) => (
-              <StoreProvider store={store}>
-                <PersistGate loading={null} persistor={persistor}>
-                  <RouterProvider route={route} />
-                </PersistGate>
-              </StoreProvider>
-            )}
-          </DynamicStoreProvider>
-        */}
+        <DynamicStoreProvider>
+          {({ store, persistor }) => (
+            <StoreProvider store={store}>
+              <PersistGate loading={null} persistor={persistor}>
+                <RouterProvider route={route} />
+              </PersistGate>
+            </StoreProvider>
+          )}
+        </DynamicStoreProvider>
       </QueryClientProvider>
     </ConfigProvider>
-  </ScreenSizeWatcher>
+  </ScreenSizeWatcher>,
 );
